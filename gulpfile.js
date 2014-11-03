d1a6ee512cccdbd18f3d0a82297a4fce183d26f3
patw <patw.hi@gmail.com>
2014-11-03 15:19:20 +0800

    Add gulp tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 var gulp = require('gulp'),
     gulpWebpack = require('gulp-webpack'),
-    webpack = require('webpack');
+    webpack = require('webpack'),
+    livereload = require('gulp-livereload');
+
+var live = livereload();
+livereload.listen();
 
 var env = 'production'; // development || production
 
@@ -9,4 +13,23 @@ gulp.task('bundle-js', function () {
       .pipe(gulpWebpack(require('./webpack.config.js')))
       .pipe(gulp.dest('./build'));
 });
+
+gulp.task('copy', function () {
+  return gulp.src(['app/index.html'], { base: 'app' })
+             .pipe(gulp.dest('./build'));
+});
+
+gulp.task('watch', function () {
+  gulp.watch('./app/**/*', ['bundle-js', 'copy', 'refresh']);
+});
+
+gulp.task('refresh', function () {
+  setTimeout(function () {
+    live.changed('');
+  }, 500);
+});
+
+gulp.task('default', ['dev']);
+
+gulp.task('dev', ['bundle-js', 'copy', 'watch']);
 
