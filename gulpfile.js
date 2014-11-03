5b62912f33c1552041bb5b404a60e9f7f72498c0
patw <patw.hi@gmail.com>
2014-11-03 16:28:03 +0800

    Add bundle CSS task to gulpfile.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,16 +1,44 @@
 var gulp = require('gulp'),
+    compass = require('gulp-compass'),
     gulpWebpack = require('gulp-webpack'),
     webpack = require('webpack'),
-    livereload = require('gulp-livereload');
+    livereload = require('gulp-livereload'),
+    notify = require('gulp-notify');
 
 var live = livereload();
 livereload.listen();
 
 var env = 'production'; // development || production
 
+gulp.task('bundle-css', function () {
+  gulp.src('./app/assets/sass/*.scss')
+    .pipe(compass({
+        style: 'compressed',
+        comments: false,
+        css: './build/assets/css',
+        sass: './app/assets/sass',
+        image: './build/assets/images',
+        font: './build/assets/fonts',
+        import_path: [
+          'bower_components/meyer-reset/stylesheets'
+        ]
+    }))
+    .on('error', function (err){
+        console.log('[錯誤]', err);
+        this.end();
+        gulp.src('').pipe(notify('✖ CSS Bundle Failed ✖'));
+    })
+    .pipe(gulp.dest('static/assets/business/css'));
+});
+
 gulp.task('bundle-js', function () {
   gulp.src('./app/js/boot.js')
       .pipe(gulpWebpack(require('./webpack.config.js')))
+      .on('error', function (err){
+          console.log('[錯誤]', err);
+          this.end();
+          gulp.src('').pipe(notify('✖ Javascript Bundle Failed ✖'));
+      })
       .pipe(gulp.dest('./build'));
 });
 
@@ -20,7 +48,7 @@ gulp.task('copy', function () {
 });
 
 gulp.task('watch', function () {
-  gulp.watch('./app/**/*', ['bundle-js', 'copy', 'refresh']);
+  gulp.watch('./app/**/*', ['bundle-js', 'bundle-css', 'copy', 'refresh']);
 });
 
 gulp.task('refresh', function () {
@@ -31,5 +59,5 @@ gulp.task('refresh', function () {
 
 gulp.task('default', ['dev']);
 
-gulp.task('dev', ['bundle-js', 'copy', 'watch']);
+gulp.task('dev', ['bundle-js', 'bundle-css', 'copy', 'watch']);
 
