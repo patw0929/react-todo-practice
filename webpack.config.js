7e5bc078a1739a4a8aaeb57600e1f937ed0ad161
patw <patw.hi@gmail.com>
2014-11-04 16:08:30 +0800

    Add loaders' settings

--- webpack.config.js
+++ webpack.config.js
@@ -15,7 +15,13 @@ module.exports = {
     loaders: [
       { test: /\.css$/, loader: "style!css" },
       { test: /\.scss$/, loader: 'style!css!sass' },
-      { test: /\.jsx$/, loader: 'jsx' }
+      { test: /\.less$/, loader: 'style!css!less' },
+      { test: /\.jsx$/, loader: 'jsx' },
+      { test: /\.woff$/, loader: 'url?limit=10000&minetype=application/font-woff' },
+      { test: /\.ttf$/, loader: 'file' },
+      { test: /\.eot$/, loader: 'file' },
+      { test: /\.svg$/, loader: 'file' },
+      { test: /\.(gif|png|jpg)$/, loaders: 'image?optimizationLevel=7&interlaced=false' }
     ]
   }
 }
